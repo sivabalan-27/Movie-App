cca8e106859f8991222fe443842084c4bb260368
sivabalan-27 <siva270410@gmail.com>
2025-09-27 23:53:08 +0530

    update home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -54,17 +54,17 @@ const Home = () => {
         className="p-2 w-3/4 md:w-1/2 border rounded border-gray-700 bg-gray-900 bg-opacity-60 text-white fixed top-16 left-1/2 transform -translate-x-1/2"
       />
 
-    <div className="grid gap-4 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 mt-32"> 
+    <div className="grid gap-4 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 mt-32 mx-10"> 
         {movies.map(movie =>{ 
             return (<Moviecard movie={movie} key={movie.title}/>); 
         } )}
     </div>
 
 
-      <div className="flex justify-between mt-5 px-5">
+      <div className="flex justify-between mt-5  mx-10">
 
         <button
-          className="p-2 bg-gray-700 text-white rounded disabled:opacity-50"
+          className="p-2 bg-gray-700 text-white rounded disabled:opacity-50 px-7 py-2 font-semibold"
           disabled={page === 1}
           onClick={() => setPage((prev) =>prev - 1)} 
         >
@@ -72,7 +72,7 @@ const Home = () => {
         </button>
 
         <button
-          className="p-2 bg-gray-700 text-white rounded"
+          className="p-2 bg-gray-700 text-white rounded px-7 py-2 font-semibold"
           onClick={() => setPage((prev) => prev + 1)}
         >
           NEXT
