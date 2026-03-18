ffd5b558037e9fe6326bf8ab401ae11500776ed5
sivabalan <siva270410@gmail.com>
2026-03-19 01:50:33 +0530

    Update GenreFilter.jsx

--- src/components/GenreFilter.jsx
+++ src/components/GenreFilter.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+const GenreFilter = ({genreList,selectedGenre}) => {
 
-const GenreFilter = ({ selectedType, setSelectedType }) => {
-  return (
-    <select
-      value={selectedType}
-      onChange={(e) => setSelectedType(e.target.value)}
-      className="p-2 bg-gray-900 bg-opacity-60 backdrop-blur-md text-white border rounded outline-none"
-    >
-      <option value="">All</option>
-      <option value="movie">Movies</option>
-      <option value="series">Series</option>
-      <option value="episode">Episodes</option>
-    </select>
-  );
-};
+    return (
+        <select className="p-2 mb-4 bg-gray-900 bg-opacity-60  backdrop-blur-md text-white border rounded" onChange={(e) => selectedGenre(e.target.value)}>
+            <option value="">All Genres</option>
+          {genreList.map((genre) => {
+            return(
+                <option key={genre.id} value={genre.id}>
+                    {genre.name}
+                </option>
+            );
+        })}  
+
+        </select>
+    );
+    }
 
 export default GenreFilter;
